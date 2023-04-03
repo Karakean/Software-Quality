7b0128f9bddfb1b23d7fd62a18e371390573f4f6
Dominik Zmuda-Trzebiatowski <dominik.zmuda_trzebiatowski@wp.pl>
2023-04-03 19:40:05 +0200

    add whitebox test

--- white_box_testing/main.py
+++ white_box_testing/main.py
@@ -1,6 +1,7 @@
 import unittest
 from send_test import SendTest
 from get_encoding_from_headers_test import TestGetEncodingFromHeaders
+from retry_test import TestRequestRetry
 
 if __name__ == '__main__':
     unittest.main()
