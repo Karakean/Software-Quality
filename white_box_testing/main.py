4178b975deae8f68fbf9356a8979b214d781e34d
Dominik Zmuda-Trzebiatowski <49036367+zmudson@users.noreply.github.com>
2023-04-05 18:33:07 +0200

    Whitebox (#7)
    * add whitebox tests for guess_json_utf
    
    * remove nieokuratne tests

--- white_box_testing/main.py
+++ white_box_testing/main.py
@@ -1,7 +1,7 @@
 import unittest
 from send_test import SendTest
 from get_encoding_from_headers_test import TestGetEncodingFromHeaders
-from retry_test import TestRequestRetry
+from guess_json_utf_test import TestGuessJsonUtf
 
 if __name__ == '__main__':
     unittest.main()
